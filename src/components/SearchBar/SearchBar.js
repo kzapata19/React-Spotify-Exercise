7c1f2a58b7573531cfdc8e51f7dfd26c81e18bfd
Karen Zapata <kzapata0919@gmail.com>
2017-04-11 21:08:53 -0700

    Fix typos

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import styles from './SeachBar.css';
+import styles from './SearchBar.css';
 
 const SearchBar = (props) => (
   <div className={styles.root}>
