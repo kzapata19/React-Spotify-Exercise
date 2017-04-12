e23d83d89d8f7f903f703961032efbbb1c099e52
Karen Zapata <kzapata0919@gmail.com>
2017-04-11 21:13:24 -0700

    Add Get Songs button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,11 +4,13 @@ import styles from './SearchBar.css';
 const SearchBar = (props) => (
   <div className={styles.root}>
     <input className={styles.input} onChange={(e) => props.updateText(e.target.value)} />
+    <button className={styles.button} onClick={props.fetchSongs}>Get Songs</button>
   </div>
 );
 
 SearchBar.propTypes = {
   updateText: PropTypes.func,
+  fetchSongs: PropTypes.func,
 };
 
 export default SearchBar;
